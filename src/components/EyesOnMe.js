7e5ed813c18f5ab31cb733695affed7a02d8117b
Tiffany Man <tiffanyman19@yahoo.coom>
2024-01-30 09:00:40 -0500

    Complete Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+
+function EyesOnMe(){
+    function HandleFocus() {
+        console.log("Good!");
+    }
+    function handleBlur(){
+        console.log("Hey! Eyes on me!");
+    }
+    return (
+        <button onFocus={HandleFocus} onBlur={handleBlur}>
+            Eyes on me
+        </button>
+    )
+}
+
+export default EyesOnMe;
